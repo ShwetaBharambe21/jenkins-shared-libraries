e0d892d7496a029351adef02445d54df2dc80e2e
Shweta Bharambe <shwetabharambe2196@gmail.com>
2026-01-30 23:33:15 +0530

    updated owasp_dependency file one more time

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,8 +1,9 @@
 def call() {
     sh '''
-      echo "Running OWASP Dependency Check..."
+      echo "Running OWASP Dependency Check (no update mode)..."
       mkdir -p owasp-report
       dependency-check.sh \
+        --noupdate \
         --scan . \
         --format ALL \
         --project wanderlust \
