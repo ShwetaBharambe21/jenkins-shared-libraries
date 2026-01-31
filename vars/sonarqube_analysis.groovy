da4728306cca713edc27055e2bc4f9ed21258905
Shweta Bharambe <shwetabharambe2196@gmail.com>
2026-01-31 14:32:11 +0530

    Changed sonarqube_analysis

---
+++ vars/sonarqube_analysis.groovy
@@ -0,0 +1,11 @@
+def call(String toolName, String projectKey, String projectName) {
+    withSonarQubeEnv(toolName) {
+        sh """
+        sonar-scanner \
+          -Dsonar.projectKey=${projectKey} \
+          -Dsonar.projectName=${projectName} \
+          -Dsonar.sources=.
+        """
+    }
+}
+
