29fb24d3e4d99807e8332568e2a89b86878f86a4
Shweta Bharambe <shwetabharambe2196@gmail.com>
2026-01-31 15:47:40 +0530

    changed

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,12 +1,11 @@
-def call(String projectKey, String projectName) {
-    sh """
-    echo "Running SonarQube scan..."
-    sonar-scanner \
-      -Dsonar.projectKey=${projectKey} \
-      -Dsonar.projectName=${projectName} \
-      -Dsonar.sources=. \
-      -Dsonar.host.url=http://<YOUR-SONAR-IP>:9000 \
-      -Dsonar.login=${SONAR_TOKEN}
-    """
+def call(String toolName, String projectKey, String projectName) {
+    withSonarQubeEnv(toolName) {
+        sh """
+          sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.sources=.
+        """
+    }
 }
 
